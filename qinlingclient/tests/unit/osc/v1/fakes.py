14d33551aabd8257c8f971470d49be2c88120ec7
Hunt Xu <mhuntxu@gmail.com>
2018-07-28 00:17:49 +0800

    Add unit tests for qinlingclient.osc.v1.function
    Story: 2001590
    Task: 24272
    
    Change-Id: I7dab6e29f22a9acaa0e992102d68eff840353887

--- qinlingclient/tests/unit/osc/v1/fakes.py
+++ qinlingclient/tests/unit/osc/v1/fakes.py
@@ -13,7 +13,9 @@
 #    limitations under the License.
 
 import copy
+import hashlib
 import mock
+import six
 import uuid
 
 from osc_lib.tests import utils
@@ -27,6 +29,7 @@ class FakeQinlingClient(object):
         self.auth_token = kwargs['auth_token']
         self.auth_url = kwargs['auth_url']
         self.runtimes = mock.Mock()
+        self.functions = mock.Mock()
 
 
 class TestQinlingClient(utils.TestCommand):
@@ -138,3 +141,89 @@ class FakeRuntime(object):
                                           loaded=True)
 
         return runtime_pool
+
+
+class FakeFunction(object):
+    """Fake one or more functions."""
+
+    @staticmethod
+    def get_fake_md5():
+        content = uuid.uuid4().hex
+        if not isinstance(content, six.binary_type):
+            content = content.encode('utf-8')
+        return hashlib.md5(content).hexdigest()
+
+    @staticmethod
+    def create_one_function(attrs=None):
+        """Create a fake function.
+
+        :param Dictionary attrs:
+            A dictionary with all atrributes
+        :return:
+            A FakeResource object, with id, name, etc.
+        """
+
+        attrs = attrs or {}
+        # Set default attributes.
+        function_attrs = {
+            'id': str(uuid.uuid4()),
+            'name': 'function-name-' + uuid.uuid4().hex,
+            'description': 'function-description-' + uuid.uuid4().hex,
+            'count': 0,
+            'code': {'md5sum': FakeFunction.get_fake_md5(),
+                     'source': 'package'},
+            'runtime_id': str(uuid.uuid4()),
+            'entry': 'main.main',
+            'project_id': str(uuid.uuid4()),
+            'created_at': '2018-07-26 09:00:00',
+            'updated_at': '2018-07-26 09:00:30',
+            'cpu': 100,
+            'memory_size': 33554432,
+        }
+
+        # Overwrite default attributes.
+        function_attrs.update(attrs)
+
+        function = fakes.FakeResource(info=copy.deepcopy(function_attrs),
+                                      loaded=True)
+
+        return function
+
+    @staticmethod
+    def create_functions(attrs=None, count=2):
+        """Create multiple fake functions.
+
+        :param Dictionary attrs:
+            A dictionary with all atrributes
+        :param int count:
+            The number of functions to fake
+        :return:
+            A list of FakeResource objects faking the functions.
+        """
+
+        functions = []
+        for i in range(count):
+            functions.append(FakeFunction.create_one_function(attrs))
+
+        return functions
+
+    @staticmethod
+    def get_functions(functions=None, count=2):
+        """Get an iterable Mock object with a list of faked functions.
+
+        If functions list is provided, then initialize the Mock object with the
+        list. Otherwise create one.
+
+        :param List functions:
+            A list of FakeResource faking functions
+        :param int count:
+            The number of functions to fake
+        :return:
+            An iterable Mock object with side_effect set to a list of faked
+            functions.
+        """
+
+        if functions is None:
+            functions = FakeFunction.create_functions(count=count)
+
+        return mock.Mock(side_effect=functions)
